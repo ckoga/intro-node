16baf4a733686514243b776cbd8d5244fd8617e3
Colin Turing <colin.koga@gmail.com>
2020-01-24 15:03:09 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+const server = http.createServer();
+
+let messages = [
+  { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
+  { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
+  { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
+];
+
+server.listen(3000, () => {
+  console.log('The HTTP server is listening at Port 3000.');
+});
+
+server.on('request', (request, response) => {
+  if (request.method === 'GET') {
+    getAllMessages(response);
+  } else if (request.method === 'POST') {
+    let newMessage = { 'id': Date.now() };
+    
+    request.on('data', (data) => {
+      newMessage = Object.assign(newMessage, JSON.parse(data));
+    });
+
+    request.on('end', () =>{
+      addMessage(newMessage, response)
+    });
+  }
+  
+  // response.writeHead(200, { 'Content-Type': 'text/plain' });
+  // response.write('Hello World\n');
+  // response.end();
+});
+
+
+const getAllMessages = (response) => {
+  response.writeHead(response.statusCode, { 'Content-Type': 'text/plain' });
+  response.write(JSON.stringify(messages));
+  response.end();
+}
+
+const addMessage = (newMsg, response) => {
+  messages.push(newMsg);
+  response.writeHead(response.statusCode, { 'Content-Type': 'text/plain' });
+  const newMessage = messages[messages.length - 1];
+  response.wrtie(JSON.stringify(newMessage));
+  response.end();
+}
